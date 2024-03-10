ae5f1171777ae86c07ea1f0c470666d8169c0981
zongyii <chadgtr34@gmail.com>
2024-03-10 14:19:33 +0100

    Update page.tsx
    rreplace some japanese word

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className={styles.container}>
       <p className={styles.text}>
-        ご質問、ご相談は下記フォームよりお問い合わせください。
+        ご質問、ご相談は下any suggestion ?
         <br />
         内容確認後、担当者より通常3営業日以内にご連絡いたします。
       </p>
